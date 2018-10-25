738a647b112c0ffe9ffcba0765fdbb25a99c3e80
Jlibbon <jplibbon@gmail.com>
2018-10-24 21:39:17 -0700

    updated

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("21280235 - This line was added by Joseph Libbon.");
+		System.out.println("212802435 - This line was added by Joseph Libbon.");
 		System.out.println("215507735 - This line was added by Catherine Nguyen.");
 		System.out.println("215392737 - This line was added by Ramiro Bazan.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
