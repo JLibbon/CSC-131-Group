3b13b644bd392e776a9021388c8217fd7ed4a8b4
bazan <bazan@10.117.3.37>
2018-10-24 21:27:34 -0700

    first commit

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("21280235 - This line was added by Joseph Libbon.");
 		System.out.println("215507735 - This line was added by Catherine Nguyen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("215392737 - This line was added by Ramiro Bazan.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
